91ff3a8f7620efbb5aba06a6cc67894509483e8e
nehasahgal <146212872+nehasahgal@users.noreply.github.com>
2023-10-22 16:16:00 +0000

    mult debugged

--- app.rb
+++ app.rb
@@ -34,7 +34,11 @@ get("/multiply") do
   erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_num = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_number").to_f
+
+  @result = @first_num*@second_num
   erb(:mult_result)
 end
 
